bf2980389972125065f2fdb9b5361fed0d6ce0e3
RPoveda25 <rafael.poveda14u@std.ui.edu.ni>
2023-05-08 00:04:09 -0600

    Incorporado Areas

--- SistemaInventarioV7/Program.cs
+++ SistemaInventarioV7/Program.cs
@@ -1,6 +1,6 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
-using SistemaInventarioV7.Data;
+using SistemaInventarioV7.AccesoDatos.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -37,7 +37,7 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{area=Inventario}/{controller=Home}/{action=Index}/{id?}");
 app.MapRazorPages();
 
 app.Run();
