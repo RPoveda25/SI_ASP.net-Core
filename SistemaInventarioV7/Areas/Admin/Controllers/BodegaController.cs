1afa34f47a52b29d870bce93210d1b2b14cea0a3
RPoveda25 <rafael.poveda14u@std.ui.edu.ni>
2023-05-11 00:06:11 -0600

    Fin Bodega CRUD

--- SistemaInventarioV7/Areas/Admin/Controllers/BodegaController.cs
+++ SistemaInventarioV7/Areas/Admin/Controllers/BodegaController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using SistemaInventarioV7.AccesoDatos.Repositorio.IRepositorio;
+using SistemaInventarioV7.Modelos;
+using SistemaInventarioV7.Utilidades;
 
 namespace SistemaInventarioV7.Areas.Admin.Controllers
 {
@@ -18,6 +20,50 @@ namespace SistemaInventarioV7.Areas.Admin.Controllers
             return View();
         }
 
+        public async Task<IActionResult> Upsert(int? id)
+        {
+            Bodega bodega = new Bodega();
+
+            if (id == null)
+            {
+                // Crear nueva bodea
+                bodega.Estado = true;
+                return View(bodega);
+            }
+
+            // Actualizar bodega
+            bodega = await _unidadTrabajo.Bodega.Obtener(id.GetValueOrDefault());
+            if (bodega == null)
+            {
+                return NotFound();
+            }
+            return View(bodega);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Upsert(Bodega bodega)
+        {
+            if (ModelState.IsValid)
+            {
+                if (bodega.Id == 0)
+                {
+                    await _unidadTrabajo.Bodega.Agregar(bodega);
+                    TempData[DS.Exitosa] = "Bodega creada Exitosamente";
+                }
+                else
+                {
+                    _unidadTrabajo.Bodega.Actualizar(bodega);
+                    TempData[DS.Exitosa] = "Bodega actualizada Exitosamente";
+                }
+                await _unidadTrabajo.Guardar();
+                return RedirectToAction(nameof(Index));
+            }
+            TempData[DS.Error] = "Error al grabar Bodega";
+            return View(bodega);
+        }
+
+
         #region API
 
         [HttpGet]
@@ -27,6 +73,41 @@ namespace SistemaInventarioV7.Areas.Admin.Controllers
 
             return Json(new {data = todos} );
         }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var bodegaDb = await _unidadTrabajo.Bodega.Obtener(id);
+            if (bodegaDb == null)
+            {
+                return Json(new { success = false, message = "Error al borrar Bodega" });
+            }
+            _unidadTrabajo.Bodega.Remover(bodegaDb);
+            await _unidadTrabajo.Guardar();
+            return Json(new { success = true, message = "Bodega borrada exitosamente" });
+        }
+
+        [ActionName("ValidarNombre")]
+        public async Task<IActionResult> ValidarNombre(string nombre, int id = 0)
+        {
+            bool valor = false;
+            var lista = await _unidadTrabajo.Bodega.ObtenerTodos();
+            if (id == 0)
+            {
+                valor = lista.Any(b => b.Nombre.ToLower().Trim() == nombre.ToLower().Trim());
+            }
+            else
+            {
+                valor = lista.Any(b => b.Nombre.ToLower().Trim() == nombre.ToLower().Trim() && b.Id != id);
+            }
+            if (valor)
+            {
+                return Json(new { data = true });
+            }
+            return Json(new { data = false });
+
+        }
+
 
         #endregion
     }
